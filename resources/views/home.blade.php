2a8ff1c78cfe07e25bd802c0be769021152199a2
Ellen Shimada <ellenshimada@gmail.com>
2019-07-03 16:21:07 -0700

    Update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,20 +17,23 @@
                     You are logged in! <br>
                     <hr><br>
                     <div>
+                        <h2>To Do</h2>
                         <ul>
                             <li>Add log for activities</li>
+                            <li>A proper welcome page</li>
                         </ul>
                     </div>
                     <div>
                         @if ($pets->count())
                         <h2>Pets</h2>
-
                         <a class="button is-link is-small" href="/pets/create">Add a pet</a>
                         <a class="button is-link is-small" href="/pets">Manage pets</a>
                         <a class="button is-link is-small" href="/activity">Manage Activities</a>
                         @foreach ($pets as $pet)
-                        <div>{{ $pet->name }}</div>
+                        <div><a href="/pets/{{ $pet->id }}" >{{ $pet->name }}</a> <a href="/pets/{{ $pet->id }}/edit" >Edit</a></div>
                         @endforeach
+                        @else
+                        <div>No furry friends here, start by <a href="/pets/create">adding</a> one.</div>
                         @endif
                     </div>
 
